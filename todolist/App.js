6948b4ca8f5354c391cdd790bb09fd140531f2d0
Man Hong Lee <manhong323@gmail.com>
2017-09-14 00:44:38 -0400

    added TaskForm and navigation

--- todolist/App.js
+++ todolist/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import TaskList from './TaskList'
+import TaskList from "./TaskList";
+import {Text} from "react-native";
+import {Navigator} from 'react-native-deprecated-custom-components';
+import TaskForm from "./TaskForm";
 
 export default class App extends React.Component {
   constructor(props, context) {
     super(props, context);
+
     this.state = {
       todos: [
         {
@@ -16,15 +20,41 @@ export default class App extends React.Component {
     };
   }
 
-  onAddStarted(){
-      console.log('on add started');
+  onAddStarted() {
+    this.nav.push({
+      name: "taskform"
+    });
+  }
+
+  renderScene(route, nav) {
+    switch (route.name) {
+      case "taskform":
+        return (
+          <TaskForm />
+        );
+      default:
+        return (
+          <TaskList
+            onAddStarted={this.onAddStarted.bind(this)}
+            todos={this.state.todos}
+          />
+        );
+    }
+  }
+
+  configureScene() {
+    return Navigator.SceneConfigs.FloatFromBottom;
   }
 
   render() {
     return (
-      <TaskList
-        onAddStarted={this.onAddStarted.bind(this)}
-        todos={this.state.todos}
+      <Navigator
+        configureScene={this.configureScene}
+        initialRoute={{ name: "tasklist", index: 0 }}
+        ref={nav => {
+          this.nav = nav;
+        }}
+        renderScene={this.renderScene.bind(this)}
       />
     );
   }
